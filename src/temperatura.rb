368a36bd0c40b372b12d169801fe3b2bac66a455
dbobrovreal <dbobrovreal@mail.ru>
2021-11-02 15:49:40 +0700

    fix temp

--- src/temperatura.rb
+++ src/temperatura.rb
@@ -1,7 +1,26 @@
-class Temperature
+class Temperatur
     def initialize(source_scale, target_scale, value)
       @source_scale = source_scale
       @target_scale = target_scale
       @value = value
     end
-end
+  
+    def converter
+      scale_map = {
+        'C' => {
+          'F' => ->(x) { (1.8 * x) + 32 },
+          'K' => ->(x) { x + 273.15 }
+        },
+        'F' => {
+          'C' => ->(x) { 0.56 * (x - 32) },
+          'K' => ->(x) { (0.56 * (x - 32)) + 273.15 }
+        },
+        'K' => {
+          'C' => ->(x) { x - 273.15 },
+          'F' => ->(x) { ((x - 273.15) * 1.8) + 32 }
+        }
+      }
+      scale_map[@source_scale][@target_scale].call(@value).round(2)
+    end
+  end
+  
